3a1c0d853819b2574d8f914cdae7667bcb9bcf82
im_abramon <im_abramon@vk.com>
2021-11-18 13:25:02 +0300

    Documentation added

--- Fusuion Engine/Fusion/Headers/Keyboard.hpp
+++ Fusuion Engine/Fusion/Headers/Keyboard.hpp
@@ -9,17 +9,46 @@
 /*!
  *\brief Class for interacting with the keyboard
  *\author imabramon
- *\version 1.0
+ *\version 0.1
  */
-
 class Keyboard{
 public:
+    /*!
+     *\brief Constructor
+     *\author imabramon
+     *\version 0.1
+     */
     Keyboard();
+    
+    /*!
+     *\brief Destructor
+     *\author imabramon
+     *\version 0.1
+     */
     ~Keyboard();
     
+    /*!
+     *\brief Method interface to the glut
+     *\author imabramon
+     *\version 0.1
+     */
     void update(unsigned char t_key, int t_x, int t_y);
     
+    /*!
+     *\brief Getting a pointer to a keypress event
+     *\param t_key Key
+     *\return Pointer to this event
+     *\author imabramon
+     *\version 0.1
+     */
     AbstractEvent * keyPressed(Key t_key);
+    
+    /*!
+     *\brief Getting a pointer to a not double press of key
+     *\return Pointer to this event
+     *\author imabramon
+     *\version 0.1
+     */
     AbstractEvent * notDoublePressed();
 private:
     std::map<Key, AbstractEvent *> m_events;
